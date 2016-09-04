f88681619a733f2d25a1f7c926e8d19b293c15d4
Gscienty <gaoxiaochuan@hotmail.com>
2016-09-05 01:58:10 +0800

    添加it

---
+++ src/blocks/font_style/it.js
@@ -0,0 +1,39 @@
+(function(root){
+    root.blocks.it = function(line_buf){
+        var self = {};
+        self.line_buf = line_buf;
+
+        this.get_block_width = function(cursor){
+            var member = cursor.get_style();
+            cursor.set_style('italic');
+            var width = self.line_buf.get_width(cursor);
+            cursor.set_style(member);
+            return width;
+        };
+
+        this.get_block_height = function(cursor){
+            var member = cursor.get_style();
+            cursor.set_style('italic');
+            var height = self.line_buf.get_height(cursor);
+            cursor.set_style(member);
+            return height;
+        };
+
+        this.block_render = function(cursor){
+            var member = cursor.get_style();
+            cursor.set_style('italic');
+            cursor.set_y(cursor.get_y() - this.get_block_height(cursor) / 2);
+            self.line_buf.render(cursor);
+            cursor.set_style(member);
+        };
+    };
+
+    root.blocks.it.prototype = root.blocks.abstract_block;
+    
+    root.blocks.it.test = function(alpha) { return /^{\\it/.test(alpha); };
+
+    root.blocks.it.build = function(alpha) {
+        const param_length = root.blocks.get_param_length(alpha);
+        return [alpha.substring(param_length + 2), new root.blocks.it((new root.line_buf()).append(alpha.substr(5, param_length - 4)))];
+    };
+})(this.latex);
