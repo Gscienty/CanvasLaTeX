f7a303633ddcae3c7fa038afa20d71f31020fdef
Gscienty <gaoxiaochuan@hotmail.com>
2016-09-05 21:05:31 +0800

    psi

---
+++ src/blocks/geek_alphabet/psi.js
@@ -0,0 +1,13 @@
+(function(root){
+    'use strict';
+
+    root.blocks.psi = {};
+    root.blocks.psi.build = function(alpha){ return root.blocks.simple('ψ' + alpha.substring(4)); };
+    root.blocks.psi.test = function(alpha) { return /^\\psi/.test(alpha); };
+
+
+    root.blocks.Psi = {};
+    root.blocks.Psi.build = function(alpha){ return root.blocks.simple('Ψ' + alpha.substring(4)); };
+    root.blocks.Psi.test = function(alpha) { return /^\\Psi/.test(alpha); };
+
+})(this.latex);
