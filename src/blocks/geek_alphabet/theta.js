96869e307f0d4eae2d162066f56f6478201be899
Gscienty <gaoxiaochuan@hotmail.com>
2016-09-05 20:43:03 +0800

    theta

---
+++ src/blocks/geek_alphabet/theta.js
@@ -0,0 +1,13 @@
+(function(root){
+    'use strict';
+
+    root.blocks.theta = {};
+    root.blocks.theta.build = function(alpha){ return root.blocks.simple('θ' + alpha.substring(6)); };
+    root.blocks.theta.test = function(alpha) { return /^\\theta/.test(alpha); };
+
+
+    root.blocks.Theta = {};
+    root.blocks.Theta.build = function(alpha){ return root.blocks.simple('Θ' + alpha.substring(6)); };
+    root.blocks.Theta.test = function(alpha) { return /^\\Theta/.test(alpha); };
+
+})(this.latex);
