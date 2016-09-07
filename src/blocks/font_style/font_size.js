1ddbb07d4474476c18d097c0e27aabffad82efcb
Gscienty <gaoxiaochuan@hotmail.com>
2016-09-07 23:54:05 +0800

    初版完成

--- src/blocks/font_style/font_size.js
+++ src/blocks/font_style/font_size.js
@@ -9,42 +9,42 @@
     };
     root.blocks.origin_font_size.prototype = root.blocks.abstract_block;
     root.blocks.tiny = {};
-    root.blocks.tiny.build = function(alpha) { return [alpha.substring(5), new root.blocks.origin_font_size(8)]; }
-    root.blocks.tiny.test = function(alpha) { return /^\\tiny/.test(alpha); }
+    root.blocks.tiny.build = function(alpha) { return [alpha.substring(5), new root.blocks.origin_font_size(8)]; };
+    root.blocks.tiny.test = function(alpha) { return /^\\tiny/.test(alpha); };
     
     root.blocks.scriptsize = {};
-    root.blocks.scriptsize.build = function(alpha) { return [alpha.substring(11), new root.blocks.origin_font_size(12)]; }
-    root.blocks.scriptsize.test = function(alpha) { return /^\\scriptsize/.test(alpha); }
+    root.blocks.scriptsize.build = function(alpha) { return [alpha.substring(11), new root.blocks.origin_font_size(12)]; };
+    root.blocks.scriptsize.test = function(alpha) { return /^\\scriptsize/.test(alpha); };
     
     root.blocks.footnotesize = {};
-    root.blocks.footnotesize.build = function(alpha) { return [alpha.substring(13), new root.blocks.origin_font_size(16)]; }
-    root.blocks.footnotesize.test = function(alpha) { return /^\\footnotesize/.test(alpha); }
+    root.blocks.footnotesize.build = function(alpha) { return [alpha.substring(13), new root.blocks.origin_font_size(16)]; };
+    root.blocks.footnotesize.test = function(alpha) { return /^\\footnotesize/.test(alpha); };
     
     root.blocks.small = {};
-    root.blocks.small.build = function(alpha) { return [alpha.substring(6), new root.blocks.origin_font_size(18)]; }
-    root.blocks.small.test = function(alpha) { return /^\\small/.test(alpha); }
+    root.blocks.small.build = function(alpha) { return [alpha.substring(6), new root.blocks.origin_font_size(18)]; };
+    root.blocks.small.test = function(alpha) { return /^\\small/.test(alpha); };
     
     root.blocks.normalsize = {};
-    root.blocks.normalsize.build = function(alpha) { return [alpha.substring(11), new root.blocks.origin_font_size(20)]; }
-    root.blocks.normalsize.test = function(alpha) { return /^\\normalsize/.test(alpha); }
+    root.blocks.normalsize.build = function(alpha) { return [alpha.substring(11), new root.blocks.origin_font_size(20)]; };
+    root.blocks.normalsize.test = function(alpha) { return /^\\normalsize/.test(alpha); };
     
     root.blocks.large = {};
-    root.blocks.large.build = function(alpha) { return [alpha.substring(6), new root.blocks.origin_font_size(22)]; }
-    root.blocks.large.test = function(alpha) { return /^\\large/.test(alpha); }
+    root.blocks.large.build = function(alpha) { return [alpha.substring(6), new root.blocks.origin_font_size(22)]; };
+    root.blocks.large.test = function(alpha) { return /^\\large/.test(alpha); };
     
     root.blocks.Large = {};
-    root.blocks.Large.build = function(alpha) { return [alpha.substring(6), new root.blocks.origin_font_size(24)]; }
-    root.blocks.Large.test = function(alpha) { return /^\\Large/.test(alpha); }
+    root.blocks.Large.build = function(alpha) { return [alpha.substring(6), new root.blocks.origin_font_size(24)]; };
+    root.blocks.Large.test = function(alpha) { return /^\\Large/.test(alpha); };
     
     root.blocks.LARGE = {};
-    root.blocks.LARGE.build = function(alpha) { return [alpha.substring(6), new root.blocks.origin_font_size(26)]; }
-    root.blocks.LARGE.test = function(alpha) { return /^\\LARGE/.test(alpha); }
+    root.blocks.LARGE.build = function(alpha) { return [alpha.substring(6), new root.blocks.origin_font_size(26)]; };
+    root.blocks.LARGE.test = function(alpha) { return /^\\LARGE/.test(alpha); };
     
     root.blocks.huge = {};
-    root.blocks.huge.build = function(alpha) { return [alpha.substring(5), new root.blocks.origin_font_size(28)]; }
-    root.blocks.huge.test = function(alpha) { return /^\\huge/.test(alpha); }
+    root.blocks.huge.build = function(alpha) { return [alpha.substring(5), new root.blocks.origin_font_size(28)]; };
+    root.blocks.huge.test = function(alpha) { return /^\\huge/.test(alpha); };
     
     root.blocks.Huge = {};
-    root.blocks.Huge.build = function(alpha) { return [alpha.substring(5), new root.blocks.origin_font_size(30)]; }
-    root.blocks.Huge.test = function(alpha) { return /^\\Huge/.test(alpha); }
+    root.blocks.Huge.build = function(alpha) { return [alpha.substring(5), new root.blocks.origin_font_size(30)]; };
+    root.blocks.Huge.test = function(alpha) { return /^\\Huge/.test(alpha); };
 })(this.latex);
---
+++ src/bundle.py
@@ -0,0 +1,64 @@
+import os
+
+file_list = ['global.js',
+             'cursor.js',
+             'line_buf.js',
+             'page.js',
+             'document.js',
+             'blocks/simple.js',
+             'blocks/section.js',
+             'blocks/paragraph.js',
+             'blocks/jump.js',
+             'blocks/font_style/bf.js',
+             'blocks/font_style/center.js',
+             'blocks/font_style/crlf.js',
+             'blocks/font_style/font_size.js',
+             'blocks/font_style/it.js',
+             'blocks/geek_alphabet/alpha.js',
+             'blocks/geek_alphabet/beta.js',
+             'blocks/geek_alphabet/chi.js',
+             'blocks/geek_alphabet/delta.js',
+             'blocks/geek_alphabet/epsilon.js',
+             'blocks/geek_alphabet/eta.js',
+             'blocks/geek_alphabet/gamma.js',
+             'blocks/geek_alphabet/iota.js',
+             'blocks/geek_alphabet/kappa.js',
+             'blocks/geek_alphabet/lambda.js',
+             'blocks/geek_alphabet/mu.js',
+             'blocks/geek_alphabet/nu.js',
+             'blocks/geek_alphabet/omega.js',
+             'blocks/geek_alphabet/phi.js',
+             'blocks/geek_alphabet/pi.js',
+             'blocks/geek_alphabet/psi.js',
+             'blocks/geek_alphabet/rho.js',
+             'blocks/geek_alphabet/sigma.js',
+             'blocks/geek_alphabet/tau.js',
+             'blocks/geek_alphabet/theta.js',
+             'blocks/geek_alphabet/upsilon.js',
+             'blocks/geek_alphabet/xi.js',
+             'blocks/geek_alphabet/zeta.js',
+             'blocks/math/dots.js',
+             'blocks/math/frac.js',
+             'blocks/math/infty.js',
+             'blocks/math/lim.js',
+             'blocks/math/prod.js',
+             'blocks/math/sum.js',
+             'blocks/math/times.js',
+             'blocks/math/to.js']
+
+result = ''
+for file_name in file_list:
+    f = open(file_name, 'r')
+    lines = f.readlines()
+    for line in lines:
+        is_spacing = False
+        for chr in line.replace('\n', '').replace('\r', ''):
+            if chr == ' ' and is_spacing == True:
+                continue
+            if chr == ' ':
+                is_spacing = True
+            else:
+                is_spacing = False
+            result = result + chr
+            
+    print result
