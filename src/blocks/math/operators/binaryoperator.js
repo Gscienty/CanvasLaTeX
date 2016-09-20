1edf09a9c90245ccc089304deb166f64e1418906
Gscienty <gaoxiaochuan@hotmail.com>
2016-09-21 01:57:20 +0800

    二元操作数

--- src/blocks/math/operators/binaryoperator.js
+++ src/blocks/math/operators/binaryoperator.js
@@ -8,4 +8,14 @@
     root.blocks.cdot = {};
     root.blocks.cdot.test = function(alpha) { return /^cdot/.test(alpha); };
     root.blocks.cdot.build = function(alpha) { return [alpha.substring(5), new root.blocks.simple('·')]; };
+
+    root.blocks.pm = {};
+    root.blocks.pm.test = function(alpha) { return /^pm/.test(alpha); };
+    root.blocks.pm.build = function(alpha) { return [alpha.substring(3), new root.blocks.simple('±')]; };
+
+    root.blocks.triangleright = {};
+    root.blocks.triangleright.test= function(alpha) { return /^triangleright/.test(alpha); };
+    root.blocks.triangleright.build = function(alpha) { return [alpha.substring(14), new root.blocks.simple('▷')]; };
+
+    
 })(this.latex);
